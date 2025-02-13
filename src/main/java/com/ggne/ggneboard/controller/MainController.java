66d2d00093cc83f3d55a9f7c1dfb49d97ed74e5f
GGongnanE <z_zzlzl@naver.com>
2025-02-13 18:00:39 +0900

    [ADD] 현재 profile이 뭔지 체크하는 코드 추가

--- src/main/java/com/ggne/ggneboard/controller/MainController.java
+++ src/main/java/com/ggne/ggneboard/controller/MainController.java
@@ -1,10 +1,20 @@
 package com.ggne.ggneboard.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
+
+    // get active profile
+    @GetMapping("/profile")
+    public String getActiveProfile() {
+        return activeProfile;
+    }
 
     // test connection
     @GetMapping("/")
