0c72a054bad1653bb8c9b75c487dd56116efccad
GGongnanE <z_zzlzl@naver.com>
2025-02-13 14:44:41 +0900

    Initial commit

---
+++ src/main/java/com/ggne/ggneboard/GgneboardApplication.java
@@ -0,0 +1,13 @@
+package com.ggne.ggneboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgneboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgneboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ggne/ggneboard/GgneboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggne.ggneboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgneboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
